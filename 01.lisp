d0d31906627b818cb4e46f557e5dc000c9785f0f
Anthony Green <green@moxielogic.org>
2023-12-01 08:58:50 -0500

    Add another version

--- 01.lisp
+++ 01.lisp
@@ -20,3 +20,18 @@
                                                                return (and (setf line (subseq line 1)) (cdr n)))
                                                        (and (let ((c (subseq line 0 1))) (setf line (subseq line 1)) c))))))
       sum (parse-integer (format nil "~A~A" (uiop:first-char nline) (uiop:last-char nline))))
+
+
+
+
+
+;; Another version of part 2
+
+(loop for line in (uiop:read-file-lines "01.input")
+      sum (let* ((nums '("zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
+                 (first (loop for n in nums minimize (or (search n line) 999)))
+                 (last (loop for n in nums maximize (or (search n line :from-end t) -1))))
+            (+ (* 10 (loop for index from 0 for string in nums
+                           when (str:starts-with? string (subseq line first)) return (mod index 10)))
+               (loop for index from 0 for string in nums
+                     when (str:starts-with? string (subseq line last)) return (mod index 10)))))
