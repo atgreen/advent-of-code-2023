5a56859f1511563eb06fc4bbf7a6b3c93098f634
Anthony Green <green@moxielogic.org>
2023-12-06 05:03:33 -0500

    Day 6

---
+++ 06.lisp
@@ -0,0 +1,15 @@
+;; Part 1
+(let* ((input (uiop:read-file-lines "06.input"))
+       (times (mapcar #'parse-integer (cdr (remove-if #'uiop:emptyp (uiop:split-string (car input))))))
+       (records (mapcar #'parse-integer (cdr (remove-if #'uiop:emptyp (uiop:split-string (cadr input)))))))
+
+  (reduce '* (loop for time in times
+                   for record in records
+                   collect (loop for x from 1 below time
+                                 when (> (* x (- time x)) record)
+                                   sum 1))))
+
+;; Part 2 - don't even bother parsing
+(loop for x from 1 below 61709066
+      when (> (* x (- 61709066 x)) 643118413621041)
+        sum 1)
