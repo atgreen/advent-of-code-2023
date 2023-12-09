4776ac536193997836a717d1e2f96ade8fed203b
Anthony Green <green@moxielogic.org>
2023-12-09 06:32:19 -0500

    New day 9

--- 09.lisp
+++ 09.lisp
@@ -1,23 +1,14 @@
-;; Part 1
-
-(loop for line in (mapcar #'uiop:split-string (uiop:read-file-lines "09.input"))
-      sum (labels ((process (nums)
-                     (let ((dnums (loop for i from 0 below (1- (length nums))
-                                        collect (- (nth (1+ i) nums) (nth i nums)))))
-                       (if (every #'zerop dnums)
-                           0
-                           (+ (car (last dnums)) (process dnums))))))
-            (let ((nline (mapcar #'parse-integer line)))
-              (+ (car (last nline)) (process nline)))))
-
-;; Part 2
-
-(loop for line in (mapcar #'uiop:split-string (uiop:read-file-lines "09.input"))
-      sum (labels ((process (nums)
-                     (let ((dnums (loop for i from 0 below (1- (length nums))
-                                        collect (- (nth (1+ i) nums) (nth i nums)))))
-                       (if (every #'zerop dnums)
-                           0
-                           (- (car dnums) (process dnums))))))
-            (let ((nline (mapcar #'parse-integer line)))
-              (- (car nline) (process nline)))))
+(labels ((solve (xform)
+           (loop for line in (mapcar #'uiop:split-string (uiop:read-file-lines "09.input"))
+                 sum (labels ((process (nums)
+                                (let ((dnums (loop for i from 0 below (1- (length nums))
+                                                   collect (- (nth (1+ i) nums) (nth i nums)))))
+                                  (if (every #'zerop dnums)
+                                      0
+                                      (+ (car (last dnums)) (process dnums))))))
+                       (let ((nline (mapcar #'parse-integer (funcall xform line))))
+                         (+ (car (last nline)) (process nline)))))))
+  ;; Part 1
+  (print (solve #'identity))
+  ;; Part 2
+  (print (solve #'reverse)))
